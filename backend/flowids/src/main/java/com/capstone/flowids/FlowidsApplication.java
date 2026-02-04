7d52a20b51eab0b56917c217b3bf5632b7aa3e55
kms0707 <kmseo070767@gmail.com>
2026-02-04 12:56:59 +0900

    feat(김민서) : spring 초기 설정

---
+++ backend/flowids/src/main/java/com/capstone/flowids/FlowidsApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.flowids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowidsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowidsApplication.class, args);
+	}
+
+}
---
+++ backend/flowids/src/test/java/com/capstone/flowids/FlowidsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.flowids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowidsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
